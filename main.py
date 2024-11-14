6c0f9da1c04d02ad31b3d4856f3ca7415bad751d
vilacaxx <davilaca15@gmail.com>
2024-11-13 20:47:24 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-#autor: Davi Vilaça Pinheiro
-#Data: 01/11/2024
-# Código: Baskara a, b, c
+#Autor: Davi Vilaça Pinheiro
+#Data: 08/11/2024
+#Teoria do IMC 
 
-# importando biblioteca matemática
-import math
+def calcula_imc(peso, altura):
+      print("Parâmetro peso:", 88)
+      print("Parâmetro altura:", 1.88)
+      imc = peso/altura**2
+      print("IMC:", imc)
+      
+#Início do programa
+print("Inicio do cálculo")
+calcula_imc(88, 1.88)
+print("Término do programa")
 
-
-a = float(input("Digite o valor de a: "))
-b = float(input("Digite o valor de b: "))
-c = float(input("Digite o valor de c: "))
-
-delta = b**2 - 4*a*c
-if delta == 0:
-    raiz1 = (-b+math.sqrt(delta))/(2*a)
-    print(" Essa equação tem a as mesmas raízes: ", raiz1)
-else:
-    if delta < 0:
-        print("A equação não tem raízes reais")
-    else:
-        raiz1 = (-b + math.sqrt(delta)) / (2 * a)
-        raiz2 = (-b-math.sqrt(delta))/(2*a)
-        print("A primeira raiz da equação é: ", raiz1)
-        print("A segunda raiz da equação é: ", raiz2)
+def calcula_imc(peso, altura):
+     print ("Parâmetro peso:", 68)
+     print("Parâmetro altura:", 1.68)
+     imc = peso/altura**2
+     print("IMC:", imc)
+     
+#Início do programa
+print("Início do cálculo")
+calcula_imc(68, 1.68)
+print("Término do programa")
